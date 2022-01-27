2b3ad2f04643fc9f96da1858ed377f61bebd0424
Justin Lange <langejustin@icloud.com>
2022-01-27 10:45:06 +0000

    Added tailwindcss, postcss, error page and new font (nunito)

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-gray-800/90 flex text-gray-100 font-nunito h-screen">
+      <div className="m-auto">
+        <h1 class="text-6xl -mt-8">⚠️</h1>
+        <h1 class="text-4xl font-black pt-8">Something went wrong</h1>
+        <h3 class="text-xl pt-4">We're working on it and we'll get it fixed as soon as we can.</h3>
+        <br /><br />
+        <a href="/" class="text-xl p-2 font-semibold text-gray-800 rounded-md px-4 py-2 bg-gray-200 shadow-lg shadow-gray-300/50 hover:bg-gray-100 transition ease-in-out duration-400">Reload Page</a>
+
+        <div class="text-gray-100 p-4 mt-16">
+          <p>Copyright © 2022 - <a href="/" class="text-gray-100">MarketGeek</a>. All rights reserved.</p>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'nunito': ['nunito', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
